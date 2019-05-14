6bdd928984d653bea97ea1318740e8f31b797c9c
archie saquilabon <asaquilabon@gmail.com>
2019-05-14 14:26:11 -0700

    button is now randomizing

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,23 +1,39 @@
-import React from 'react';
+import React from "react";
 
-const foodChoices=['filipino', 'korean bbq', 'bbq', 'sushi', 'ramen', 'mexican', 'hawaiian'] 
-const chosen = (arr) => {
-  const item = arr[Math.floor(Math.random() * arr.length)]
-  console.log(item)
-}
- class Button extends React.Component {
-  
-  state = {}
-  
-  handleClick = () => {
-    chosen(foodChoices)
-  }
-  
+const foodChoices = [
+  "filipino",
+  "korean bbq",
+  "bbq",
+  "sushi",
+  "ramen",
+  "mexican",
+  "hawaiian"
+];
+const chosen = arr => {
+  const item = arr[Math.floor(Math.random() * arr.length)];
+  return item;
+};
+
+class Button extends React.Component {
+  state = {
+    item: "Click to decide what to eat!"
+  };
+
+  handleClick = event => {
+    event.preventDefault();
+    this.setState({
+      item: chosen(foodChoices)
+    });
+  };
+
   render() {
     return (
-      <button onClick={this.handleClick}>
-        Random Food Button! 
-      </button>
+      <div>
+        <button onClick={this.handleClick}>Random Food Button!</button>
+        <div>
+          <h1>{this.state.item}</h1>
+        </div>
+      </div>
     );
   }
 }
