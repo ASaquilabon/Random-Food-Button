56d83be61dda1629e0504ec3ce186fb24d535c5e
archie saquilabon <asaquilabon@gmail.com>
2019-05-16 12:51:30 -0700

    pushing

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { faUtensils, faSearchLocation, faPhone, faMapMarkerAlt} from '@fortaweso
 import Button from './components/button';
 import List from './components/list/List.jsx';
 import './App.css';
+import axios from 'axios';
 
 const chosen = arr => {
   const item = arr[Math.floor(Math.random() * arr.length)];
@@ -29,12 +30,31 @@ class App extends Component {
       searchLocationQuery: "irvine"
     };
   }
-
-  handleClick = event => {
+  
+   handleClick = event => {
     event.preventDefault();
     this.setState({
       item: chosen(foodChoices)
+    }, ()=> {
+      
     });
+    axios.get(`${'https://cors-anywhere.herokuapp.com/'}https://api.yelp.com/v3/businesses/search?location=${this.state.searchLocationQuery}&term=${this.state.item}`, {
+      //required authorization format from API 
+      headers: {
+          //to get the API from the .env file use process.env.{variable name}
+          Authorization: 'Bearer 4PtF46O0ShJUAsA7FLHtACoydrlyDAaG_MYntvijYDCUGq4GebvL8jhkojNp-1__mJlYDKHAUPIz9Moo90k3LSjEEcr-RyYXipe5TK1sx-dLtM5G-Y4I0_E2y8vRXHYx'
+      },
+      })
+      .then((res) => {
+          console.log(res.data.businesses)
+          //change the state of App to reflect on the result we are given from the API
+          //at the same time, setting the loading state to false 
+          this.setState({ results: res.data.businesses, loading: false })
+      })
+      .catch((err) => {
+          //fire the errorState message if there is no information return from the API
+          this.setState({ errorState: `Sorry we coudln't find information related to the location you search, do you want to try something else?`, loading: false })
+      })
   };
 
   render() {
@@ -46,7 +66,7 @@ class App extends Component {
        />
         <List 
           searchLocationQuery = {this.state.searchLocationQuery}
-          foodChoice = {this.state.item}
+          item = {this.state.item}
           /> 
       </div>
     );
--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -33,24 +33,6 @@ class List extends Component {
 
         //using a proxy server cors-anywhere to get rid of the CROS probblem 
         //SUPER HOT TIP: passing the location variable, which equals to the user's input (see below). Instead of grabbbing the entire API, it will only retrieve the restaurants that are closed to the lcoation information we entered. This makes the lodading wayyyyyyy faster.
-        axios.get(`${'https://cors-anywhere.herokuapp.com/'}https://api.yelp.com/v3/businesses/search?location=${this.props.searchLocationQuery}&term=${this.props.foodChoice}`, {
-        //required authorization format from API 
-        headers: {
-            //to get the API from the .env file use process.env.{variable name}
-            Authorization: 'Bearer 4PtF46O0ShJUAsA7FLHtACoydrlyDAaG_MYntvijYDCUGq4GebvL8jhkojNp-1__mJlYDKHAUPIz9Moo90k3LSjEEcr-RyYXipe5TK1sx-dLtM5G-Y4I0_E2y8vRXHYx'
-        },
-
-        })
-        .then((res) => {
-            console.log(res.data.businesses)
-            //change the state of App to reflect on the result we are given from the API
-            //at the same time, setting the loading state to false 
-            this.setState({ results: res.data.businesses, loading: false })
-        })
-        .catch((err) => {
-            //fire the errorState message if there is no information return from the API
-            this.setState({ errorState: `Sorry we coudln't find information related to the location you search, do you want to try something else?`, loading: false })
-        })
     }
 
     renderEmptyState () {
