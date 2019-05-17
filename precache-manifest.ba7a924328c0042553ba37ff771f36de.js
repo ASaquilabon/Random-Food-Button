1b8821cdd4c02fc9b5ae4f19427d7e968190459e
archie saquilabon <asaquilabon@gmail.com>
2019-05-17 10:48:21 -0700

    Updates

---
+++ precache-manifest.ba7a924328c0042553ba37ff771f36de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3704dde40abf53f595af822f790d4a20",
+    "url": "/Random-Food-Button/index.html"
+  },
+  {
+    "revision": "799005ace7cc88ff791b",
+    "url": "/Random-Food-Button/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "a7c0d35cd9326ce23a99",
+    "url": "/Random-Food-Button/static/js/2.14fb1631.chunk.js"
+  },
+  {
+    "revision": "799005ace7cc88ff791b",
+    "url": "/Random-Food-Button/static/js/main.b8d71603.chunk.js"
+  },
+  {
+    "revision": "dc1563381a7fa228f5a0",
+    "url": "/Random-Food-Button/static/js/runtime~main.aaaa0989.js"
+  }
+]);
