d1b0068f439b593fa7253ddb8ccc808d787c7dd1
archie saquilabon <asaquilabon@gmail.com>
2021-11-30 19:26:05 -0800

    Updates

---
+++ precache-manifest.f7370547c1e1c603b9cc89f7f71f76dc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b137073d0a3d7913f181b69ca35288b6",
+    "url": "/Random-Food-Button/index.html"
+  },
+  {
+    "revision": "ae60c260ba2462274288",
+    "url": "/Random-Food-Button/static/css/main.dd628208.chunk.css"
+  },
+  {
+    "revision": "3962ecfcc39b6e954988",
+    "url": "/Random-Food-Button/static/js/2.22fa5743.chunk.js"
+  },
+  {
+    "revision": "b81f6ac5aaef396470371562f28e25c2",
+    "url": "/Random-Food-Button/static/js/2.22fa5743.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae60c260ba2462274288",
+    "url": "/Random-Food-Button/static/js/main.72d01047.chunk.js"
+  },
+  {
+    "revision": "e71bce15d610a1ad0322",
+    "url": "/Random-Food-Button/static/js/runtime-main.2e041593.js"
+  },
+  {
+    "revision": "b1dde36d57c6f679e4f3251eeca5fded",
+    "url": "/Random-Food-Button/static/media/Logo.b1dde36d.png"
+  },
+  {
+    "revision": "0436cb2b79037c65ce3f47b4596cffcb",
+    "url": "/Random-Food-Button/static/media/hawaii.0436cb2b.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Random-Food-Button/precache-manifest.cc9fc4eef8bf5994760e25ba933b9438.js"
+  "/Random-Food-Button/precache-manifest.f7370547c1e1c603b9cc89f7f71f76dc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Random-Food-Button/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
